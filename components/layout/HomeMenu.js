b6d0c82285cb65a54a1154499f9539edfd2f2212
Sashitha <user@DESKTOP-6H9VGAS>
2024-05-15 00:07:52 +0530

    change image pizza

--- components/layout/HomeMenu.js
+++ components/layout/HomeMenu.js
@@ -1,18 +1,18 @@
-import Image from "next/image";
+ import Image from "next/image";
 
 export default function HomeMenu() {
     return (
         <section className="">
-            <div>
-                <div className="h-48 relative">
-                  <Image src={'/sallad1.jpg'} layout={'fill'}
-                   objectFit={'contain'} alt={'sallad'} /> 
+           <div classname="absolute left-0 right-0 w-full justify-start overflow-x-hidden">
+                <div className=" absolute text-left">
+                  <Image src={'/sallad1.jpg'} width={109} height={189} 
+                   alt={'sallad'} /> 
                 </div>
-                <div className="h-48 relative">
-                  <Image src={'/sallad1.jpg'} layout={'fill'} 
-                  objectFit={'contain'} alt={'sallad'} /> 
+                <div className=" absolute right-[180px] z-10">
+                  <Image src={'/sallad1.jpg'} width={107} height={195} 
+                  alt={'sallad'} /> 
                 </div>
-            </div>
+           </div>
             <div className="text-center">
               <h3 className="uppercase text-gray-500 font-semibold
               leading-4">
